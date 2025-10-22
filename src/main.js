0c5c45916e85969ab638fabac1afe439ccfd8f69
ki8yk8 <ki8***@gmail.com>
2025-10-22 14:18:38 +0545

    clamps the position of basket

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import kaplay from "kaplay";
 
-const k = kaplay({});
+const k = kaplay();
 
 k.loadRoot("./");
+
+const PLAYER_SPEED = 400;
 
 // creating basket and registering the handle buttons
 const basket = k.add([
@@ -11,12 +13,16 @@ const basket = k.add([
 	k.anchor("bot"),
 ]);
 
-basket.onKeyDown("left", () => {
-	basket.move(-400, 0);
-});
-basket.onKeyDown("right", () => {
-	k.debug.log(k.pos)
-	basket.move(400, 0);
+k.onUpdate(() => {
+	// check if there was a key movement
+	if (k.isKeyDown("left")) basket.move(-1 * PLAYER_SPEED, 0);
+	if (k.isKeyDown("right")) basket.move(PLAYER_SPEED, 0);
+
+	// clamping logic here
+	const [b_x, b_y] = [k.width(), k.height()]; // boundary
+	const [p_x, p_y] = [basket.pos.x, basket.pos.y]; // basket
+
+	basket.pos.x = k.clamp(p_x, basket.width / 2, b_x - basket.width / 2);
 });
 
 k.debug.log(`Screen width and height = (${k.width()}, ${k.height()})`);
