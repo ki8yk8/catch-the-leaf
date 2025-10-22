1e8388b0e48498fb83996fa3c9a1c0d3962a1e23
ki8yk8 <ki8***@gmail.com>
2025-10-22 14:30:19 +0545

    creates falling object

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const k = kaplay();
 k.loadRoot("./");
 
 const PLAYER_SPEED = 400;
+const GRAVITY = 50;
+
+k.setGravity(GRAVITY); // global gravity
 
 // creating basket and registering the handle buttons
 const basket = k.add([
@@ -23,6 +26,25 @@ k.onUpdate(() => {
 	const [p_x, p_y] = [basket.pos.x, basket.pos.y]; // basket
 
 	basket.pos.x = k.clamp(p_x, basket.width / 2, b_x - basket.width / 2);
+});
+
+// randomly spawn leaf at the top
+function spawn_leaf(size = [25, 25]) {
+	const leaf = k.add([
+		k.rect(...size),
+		k.pos(k.rand(0, k.width()), 0), // randomly spawn from the top position
+		k.area(), // for collision
+		k.body(), // for gravity
+		"leaf",
+		"leaf--falling",
+	]);
+
+	k.debug.log(`Leaf spawned at ${leaf.pos.x} ${leaf.pos.y}`);
+}
+
+// spawn a leaf every 2 seconds
+k.loop(2, () => {
+	spawn_leaf();
 });
 
 k.debug.log(`Screen width and height = (${k.width()}, ${k.height()})`);
