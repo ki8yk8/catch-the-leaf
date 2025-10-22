347ead2d22fc0098a9ab6152e64702c96a334b0a
ki8yk8 <ki8***@gmail.com>
2025-10-22 13:45:35 +0545

    learns about kaplay

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 import kaplay from "kaplay";
 
-const k = kaplay();
+const k = kaplay({
+	background: "#d46eb3",
+});
 
 k.loadRoot("./");
-k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+// working with objects
+const basket = k.add([
+	k.rect(32, 32),
+	k.pos(10, 20),
+	k.area(),
+	k.body(),
+	"shape",
+]);
+k.setGravity(10);
+k.onKeyDown("right", () => {
+	// basket.move(200, 0);
+	basket.jump();
+});
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+// working with scenes
+// k.loadBean();
+// k.scene("scenename", () => {
+// 	k.add([k.sprite("bean"), k.color(k.RED)]);
+// 	k.onKeyPress(() => {
+// 		k.go("bluebean");
+// 	});
+// });
+
+// k.scene("bluebean", () => {
+// 	k.add([k.sprite("bean"), k.color(k.BLUE)]);
+// });
+
+// k.go("scenename");
