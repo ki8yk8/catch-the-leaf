f877f27e2d58560ae7d58dad56eba551010d0f40
ki8yk8 <ki8***@gmail.com>
2025-10-23 19:17:28 +0545

    fixes the width of game to make it playable on large screens too

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import { registerStartScene } from "./scenes/start-game";
 import { registerGameOverScene } from "./scenes/gameover";
 
 const k = kaplay({
+	canvas: document.getElementById("game"),
 	background: "#fdfffc",
+	width: 600,
+	height: window.innerHeight,
+	scale: 1,
+	stretch: false,
 	global: false,
 });
 
@@ -14,7 +19,6 @@ k.loadSprite("heart", "/sprites/heart.png");
 k.loadSprite("steel", "/sprites/steel.png");
 k.loadSprite("grass", "/sprites/grass.png");
 k.loadSprite("leaf", "/sprites/leaf.png");
-
 
 if (process.env.NODE_ENV === "development") {
 	k.debug.inspect = true;
