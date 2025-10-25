c4a16e4530351eb91d631dbd1c9a20f690c9a44a
ki8yk8 <ki8***@gmail.com>
2025-10-25 13:45:34 +0545

    styles the gameover screen

--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -1,40 +1,54 @@
-export function registerGameOverScene({ k }) {
+export function registerGameOverScene({ k, padding }) {
 	k.scene("gameover", (score) => {
 		// play the failed sound
 		k.play("gameover");
+		
+		const game_screen = k.add([
+			k.rect(k.width() - 2 * padding, k.height()),
+			k.anchor("topleft"),
+			k.pos(padding, 0),
+			k.color("#86DB3C"),
+		]);
 
-		const score_title = k.add([
+		const score_title = game_screen.add([
 			k.text("You Scored", {
-				size: 18,
+				font: "atma-bold",
+				size: 32,
 				align: "center",
 			}),
 			k.anchor("center"),
-			k.pos(k.width() / 2, k.height() / 3),
+			k.pos(game_screen.width / 2, game_screen.height / 3),
 			k.color("#000000"),
 		]);
 
-		const score_text = k.add([
+		const score_text = game_screen.add([
 			k.text(score.toString(), {
-				size: 64,
+				size: 100,
 				align: "center",
 			}),
 			k.anchor("top"),
-			k.pos(k.width() / 2, score_title.pos.y + 16),
-			k.color("#00ff00"),
+			k.pos(game_screen.width / 2, score_title.pos.y + 32),
+			k.color("#ffffff"),
 		]);
-
-		const continue_text = k.add([
-			k.text("Press any key to continue", {
-				size: 18,
-				align: "center",
+		
+		const hint_text = game_screen.add([
+			k.text("Press any key to go back...", {
+				font: "atma-bold",
 			}),
-			k.anchor("top"),
-			k.pos(k.width() / 2, k.height() / 3 + 18 + 64 + 16),
-			k.color("#ff0000"),
+			k.anchor("bot"),
+			k.pos(game_screen.width / 2, game_screen.height - 100),
+			k.animate({
+				relative: true,
+			}),
 		]);
 
-		k.onKeyPress(() => {
-			k.go("startgame");
+		// bouncy effect of hint text
+		hint_text.animate("scale", [k.vec2(1), k.vec2(1.05), k.vec2(1)], {
+			duration: 1,
+			easing: k.easings.easeInBounce,
 		});
+
+		k.onKeyPress(() => k.go("startgame"));
+
 	});
 }
